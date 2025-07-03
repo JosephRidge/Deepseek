b29bf606e2eb43b971ed25b5fb02f12b426197e9
JosephRidge <josephridge18@gmail.com>
2025-07-03 18:33:37 +0300

    Update

--- app/src/main/java/com/jayr/deepseek/ui/screens/PlacePage.kt
+++ app/src/main/java/com/jayr/deepseek/ui/screens/PlacePage.kt
@@ -1,11 +1,13 @@
 package com.jayr.deepseek.ui.screens
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -37,6 +39,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -209,12 +212,27 @@ fun PlacePage(navController: NavHostController) {
                                     )
                                 }
                             }
-                    }
-
-
-
-
+                        }
                 }
+                    Spacer(
+                        modifier = Modifier.padding(vertical = 10.dp)
+                    )
+                    Text(
+                        text = "Description",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(vertical = 2.dp)
+                    )
+
+                    Text(
+                        text =  stringResource(R.string.dummy_text),
+                        fontSize = 12.sp,
+                        color = Color.Gray,
+                        modifier = Modifier.padding(vertical = 2.dp)
+                    )
+                    Row {
+
+                    }
 
             }
 
